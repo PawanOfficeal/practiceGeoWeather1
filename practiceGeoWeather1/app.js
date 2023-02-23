490d5b3a25fa777f19433e005ebd194cb4c2066c
PawanOfficeal <pawanOfficeal@gmail.com>
2023-02-23 11:20:22 -0600

    commit 1

--- practiceGeoWeather1/app.js
+++ practiceGeoWeather1/app.js
@@ -1,5 +1,9 @@
 //get geoLocation
+let showDataPara = document.getElementById("showDataPara")
 let failureMsgPara = document.getElementById("failureMsgPara")
+let positionPara = document.getElementById("positionPara")
+let showData = document.getElementById("showData")
+let mainClass = document.getElementById("mainClass")
 window.addEventListener("DOMContentLoaded",geoLocationFunction)
 
 function geoLocationFunction(){
@@ -12,8 +16,13 @@ function geoLocationFunction(){
 }
 
 function success(position){
-    const longitude = position.coords.longitude
+
     const latitude = position.coords.latitude
+    const longitude = position.coords.longitude
+    
+    positionPara.innerHTML = `${latitude} ${longitude}`
+    positionPara.style.backgroundColor = 'lightpink'
+    getWeather(latitude, longitude);
 }
 
 function failure(err){
@@ -36,3 +45,57 @@ function failure(err){
 }
 
 //get weather info
+   
+
+ // window.addEventListener("DOMContentLoaded",getWeather)
+
+  function getWeather(latitude,longitude){
+    const URL= 
+    `http://api.openweathermap.org/data/2.5/forecast?lat=${latitude}&lon=${longitude}&appid=${apikey}`
+    fetch(URL)
+    .then(response=>{
+        console.log(response)
+        return response.json()
+    })
+    .then(data =>{
+        console.log(data)
+
+        //for main div to display in header:
+        let showInBigDiv = document.getElementById("showInBigDiv")
+        let bigDiv = document.createElement("div")
+        bigDiv.innerHTML = `
+                                 <p class="mainDiv"> ${data.city.name}</p>
+                                 <p class="mainDiv"> ${(data.list[0].main.temp_max-273).toFixed(0)} &deg;C</p>
+                                  `
+                                  showInBigDiv.appendChild(bigDiv)
+        
+        for(let i = 0; i < data.list.length;i= i+8){
+                
+            console.log(data.list[i])
+            
+                let time = new Date(data.list[i].dt*1000)
+                console.log(time)
+                let day = time.toString().split("")[0]
+                
+                console.log(day)
+            
+              let showData = document.getElementById("showData")
+              let div = document.createElement("div")
+              div.innerHTML = `
+                       
+                        <p> ${day}</p>
+                        <p class="max-temp tempPara">Max-Temp:${(data.list[i].main.temp_max-273).toFixed(0)} &deg;C</p>
+                                
+              `
+              showData.appendChild(div)
+              
+
+            
+            
+        }
+        
+    })
+    .catch(err=>{
+        console.log(err)
+    })
+}
--- practiceGeoWeather1/config.js
+++ practiceGeoWeather1/config.js
@@ -1,8 +1,7 @@
 // config.js
-const config1dayKey = { // this should be const instead if you're using ES6 standards
-    apiKey : `bb218504172d9d6b50d2f9de877c1a67`
-}
+
 
 const config5dayKey = { // this should be const instead if you're using ES6 standards
-    apiKey : 'bb218504172d9d6b50d2f9de877c1a67'
+    apiKey5 : 'bb218504172d9d6b50d2f9de877c1a67'
 }
+const apikey = 'bb218504172d9d6b50d2f9de877c1a67'
